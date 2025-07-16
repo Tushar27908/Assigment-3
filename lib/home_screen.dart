cca31db946f47c25b10e67bf10242a448d8039ed
Tushar27908 <tushargupta@gmail.com>
2025-07-16 22:40:16 +0530

    Create a container which changes color on button tap

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,56 +10,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int leftdice = 4;
-  int rightdice = 2;
+  // Step 1 : Create list to select the colors from
+  List<Color> colors = [
+    Colors.red,
+    Colors.blue,
+    Colors.purple,
+    Colors.black,
+    Colors.orange,
+    Colors.white,
+    Colors.teal,
+    Colors.blueGrey,
+    Colors.yellow,
+  ];
+
+  // Step 2: Create a variable to store the selected Color
+  Color? selectedColor;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-            child: Text(
-          "DICEE",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-        )),
-        backgroundColor: Colors.pink,
+        title: Text("Change Color"),
       ),
-      backgroundColor: Colors.grey,
-      body: GestureDetector(
-        onTap: () {
-          var random1 = Random().nextInt(5) + 1;
-          var random2 = Random().nextInt(5) + 1;
-          debugPrint(random1.toString());
-          setState(() {
-            leftdice = random1;
-            rightdice = random2;
-          });
-        },
-        child: Center(
-          child: Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image(
-                    image: AssetImage('images/dice$leftdice.png'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image(
-                    image: AssetImage('images/dice$rightdice.png'),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        // Step 3: Get a random no. from (0 to length of list-1)
+        final randomNumber = Random().nextInt(colors.length);
+
+        // Step4: Update the selectedColor variable and call setstate
+        setState(() {
+          selectedColor = colors[randomNumber];
+        });
+      }),
+      body: Center(
+        child: Container(
+            height: 150, width: 150, color: selectedColor ?? Colors.black),
       ),
     );
   }
